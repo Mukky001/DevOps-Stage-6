613b5740682534274e7fdc5cdfd8b485e9f173f0
Ubuntu <ubuntu@ip-172-31-20-205.eu-north-1.compute.internal>
2025-11-29 08:15:43 +0000

    fix:final update and fixes

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    # ⚠️ ACTION REQUIRED: Update this bucket name!
+    # Update this bucket name!
     bucket = "devops-stage6-task"
     key    = "stage6/terraform.tfstate"
     region = "eu-north-1"
